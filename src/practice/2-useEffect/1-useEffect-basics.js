da795f3fd859cce81c337f92f1fd3841c8031e92
NomanShahid <noman.shahid@regbits.com>
2021-01-07 22:01:53 +0500

    Basic of useEffect hook

--- src/practice/2-useEffect/1-useEffect-basics.js
+++ src/practice/2-useEffect/1-useEffect-basics.js
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from "react";
 // by default runs after every re-render
 // cleanup function
 // second parameter
+// You can have multiple useEffect in components
 const UseEffectBasics = () => {
   const [value, setValue] = useState(0);
+
+  // useEffect Second Parameter is the list of dependencies
   useEffect(() => {
     document.title = `New Messages (${value})`;
-  });
+  }, [value]);
   return (
     <>
       <h2>{value}</h2>
