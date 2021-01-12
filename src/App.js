c01e8bea46b6f05982f60dc46707458dbbb5173e
NomanShahid <noman.shahid@regbits.com>
2021-01-12 10:22:59 +0500

    Form and controlled input

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import UseStateBasics from "./practice/1-useState/2-useState-basics";
 import UseStateArray from "./practice/1-useState/3-useState-array";
 import UseStateObject from "./practice/1-useState/4-useState-object";
 import UseStateCounter from "./practice/1-useState/5-useState-counter";
-import Project from "./practice/1-useState/Project/Project";
 import UseEffectBasics from "./practice/2-useEffect/1-useEffect-basics";
 import UseEffectCleanup from "./practice/2-useEffect/2-useEffect-cleanup";
 import UseEffectFetchData from "./practice/2-useEffect/3-useEffect-fetch-data";
 import MultipleReturns from "./practice/3-conditional-rendering/1-multiple-returns";
-import ShortCircuit from './practice/3-conditional-rendering/2-short-circuit'
-import ShowHide from './practice/3-conditional-rendering/3-show-hide'
+import ShortCircuit from "./practice/3-conditional-rendering/2-short-circuit";
+import ShowHide from "./practice/3-conditional-rendering/3-show-hide";
+import ControlledInputs from "./practice/4-forms/1-controlled-inputs";
 function App() {
   return (
     <div className="container">
@@ -19,13 +19,13 @@ function App() {
       <UseStateArray />
       <UseStateObject />
       <UseStateCounter />
-      <Project />
       <UseEffectBasics />
       <UseEffectCleanup />
       <UseEffectFetchData />
       <MultipleReturns />
-      <ShortCircuit/>
-      <ShowHide/>
+      <ShortCircuit />
+      <ShowHide />
+      <ControlledInputs />
     </div>
   );
 }
--- src/practice/4-forms/1-controlled-inputs.js
+++ src/practice/4-forms/1-controlled-inputs.js
@@ -1,12 +1,72 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // JS
 // const input = document.getElementById('myText');
 // const inputValue = input.value
 // React
 // value, onChange
-
+// Controlled input means you have value at input which will be pointing at value in state
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+  const [firstName, setFirstName] = useState("");
+  const [email, setEmail] = useState("");
+  const [people, setPeople] = useState([]);
+  const handleSumit = (e) => {
+    e.preventDefault();
+    if (firstName && email) {
+      setPeople(() => {
+        const person = {
+          id: new Date().getTime().toString(),
+          firstName,
+          email,
+        };
+        return [...people, person];
+      });
+      setFirstName("");
+      setEmail("");
+    } else {
+      console.log("No values in the form, Submit Fail!!!");
+    }
+  };
+  return (
+    <>
+      <article>
+        <h1>controlled inputs</h1>
+        <form className="form" onSubmit={handleSumit}>
+          <div className="form-control">
+            <label htmlFor="firstName">Name : </label>
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="email">Email : </label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <button type="submit">add person</button>
+        </form>
+        {people.map((peo) => {
+          const { id, firstName, email } = peo;
+          return (
+            <>
+              <div className="item" key={id}>
+                <h1>{firstName}</h1>
+                <p>{email}</p>
+              </div>
+            </>
+          );
+        })}
+      </article>
+    </>
+  );
 };
 
 export default ControlledInputs;
